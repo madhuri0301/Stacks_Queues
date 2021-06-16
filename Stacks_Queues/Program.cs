04361fcee1689e4aa1b425771254aada81d24733
Madhuri <harshupatil0301@gmail>
2021-06-16 22:42:43 +0530

    [Madhuri]Add : Welcome Message Uploaded

---
+++ Stacks_Queues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stacks_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Stacks And Queues Program");
+        }
+    }
+}
